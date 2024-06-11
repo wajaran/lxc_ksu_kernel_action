a33da926756283316a0962b0f122d51ebb4d80f9
wajaran <95926935+wajaran@users.noreply.github.com>
2024-06-12 01:18:21 +0800

    Update LXC-DOCKER-OPEN-CONFIG.sh

--- patches/LXC-DOCKER-OPEN-CONFIG.sh
+++ patches/LXC-DOCKER-OPEN-CONFIG.sh
@@ -211,8 +211,8 @@ for c in $CONFIGS_ON;do
 done
 
 for c in $CONFIGS_EQ;do
-	lhs=$(awk -F= '{ print $1 }' <(echo $c))
-	rhs=$(awk -F= '{ print $2 }' <(echo $c))
+	lhs=$(bash -c 'awk -F= '{ print $1 }' <(echo $c)')
+	rhs=$(bash -c 'awk -F= '{ print $2 }' <(echo $c)')
 	if grep "^$c" "$FILE" >/dev/null;then
 		egreen "$c is already set correctly."
 		continue
